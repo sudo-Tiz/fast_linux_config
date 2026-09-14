68b31aa13aaf6a73a06bbc8fd2dd7139d667473c
sudo-Tiz <mathis.worksome@simplelogin.fr>
2026-09-14 15:41:50 +0200

    feat: Add tmux; refactor nvim; adapt Readme

--- README.md
+++ README.md
@@ -2,45 +2,27 @@
 
 > **Minimal dotfiles to quickly setup a new Linux environment**
 
-Lightweight and efficient configurations for Neovim, Zsh, and Bash.
+Lightweight and efficient configurations for Neovim, Tmux, Zsh, and Bash.
 
 ## 📦 Quick Setup
 
 ```bash
 # Install essentials
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y zsh neovim curl git
+sudo apt install -y zsh neovim curl git tmux
 
-# Get configs
-curl -LO https://raw.githubusercontent.com/sudo-Tiz/fast_linux_config/main/.zshrc
+# Nvim 
 curl --create-dirs -Lo ~/.config/nvim/init.vim \
   https://raw.githubusercontent.com/sudo-Tiz/fast_linux_config/main/init.vim
 
-# Switch to zsh
-chsh -s zsh && zsh
-```
+# Tmux
+curl --create-dirs -Lo ~/.config/tmux/tmux.conf \
+  https://raw.githubusercontent.com/sudo-Tiz/fast_linux_config/main/tmux.conf
 
-## ✨ Features
-
-### Neovim
-
-- Auto-installs vim-plug and essential plugins
-- NERDTree, vim-surround, commentary, airline, vimagit
-- Vi mode, split navigation, auto-cleanup
-
-### Zsh/Bash
-
-- Colorful prompt: `[user@host path]$`
-- Vi mode with adaptive cursor
-- 10M history with deduplication
-- 50+ git/python/nav aliases
-- Syntax highlighting
-
-### Bash Alternative
-
-Don't want to switch to Zsh? Use the Bash version instead:
-
-```bash
+# Bash
 curl -LO https://raw.githubusercontent.com/sudo-Tiz/fast_linux_config/main/.bashrc
-```
 
+# Zsh
+curl -LO https://raw.githubusercontent.com/sudo-Tiz/fast_linux_config/main/.zshrc && \
+    chsh -s zsh && zsh
+```
---
+++ init.lua
@@ -0,0 +1,119 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- NvChad settings
+package.preload.chadrc = function()
+  return { base46 = { theme = "onedark" } }
+end
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = { formatters_by_ft = { lua = { "stylua" } } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      vim.lsp.enable { "html", "cssls" }
+    end,
+  },
+  { "windwp/nvim-autopairs", enabled = false },
+  { import = "nvchad.blink.lazyspec" },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        list = { selection = { preselect = false, auto_insert = false } },
+      },
+    },
+  },
+}, {
+  defaults = { lazy = true },
+  install = { colorscheme = { "nvchad" } },
+  ui = {
+    icons = {
+      ft = "",
+      lazy = "󰂠 ",
+      loaded = "",
+      not_loaded = "",
+    },
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+      },
+    },
+  },
+})
+
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+-- Options and autocommands
+require "nvchad.options"
+vim.opt.spelllang = { "fr" }
+
+require "nvchad.autocmds"
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function(data)
+    if vim.fn.isdirectory(data.file) == 1 then
+      vim.cmd.cd(data.file)
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
+
+-- Keymaps
+vim.schedule(function()
+  require "nvchad.mappings"
+  local map = vim.keymap.set
+  map("n", ";", ":", { desc = "CMD enter command mode" })
+  map("i", "jk", "<ESC>")
+  map({ "n", "t" }, "<leader>S", ":%s//g<Left><Left>")
+  map("n", "<leader>cc", function()
+    vim.b.cmp_enabled = not vim.b.cmp_enabled
+    require("cmp").setup.buffer { enabled = vim.b.cmp_enabled }
+  end, { desc = "Toggle cmp" })
+end)
